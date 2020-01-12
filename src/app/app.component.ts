cffa4bedd8625f31688b6bfa04add93e89325afe
daniellondonosanchez <daniellondonosanchez@hotmail.com>
2020-01-12 09:36:52 -0500

    App estable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiserviceService } from './servicios/apiservice.service';
 
+// import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
